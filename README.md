c34a64597ef562c4594718024e09598141dd7925
Raskolnikov <jerry.tann@opteyes.cn>
2020-12-27 21:06:14 +0800

    feat: update readme

--- README.md
+++ README.md
@@ -8,3 +8,13 @@
   0. yarn
   1. nav to src folder
   2. node ./controller/server.js
+
+### explanation
+
+  1.  since the free version of mongodb does not support db at rest encryption, application level encryption was used, using node native crypto library
+  2.  for preparing files for download, a temp folder was created.  The files written here should be cleaned after being served to the client.
+
+### TODO
+
+  1.  unit tests
+  2.  deployment (dockerfile, k8s yamls)
--- src/security/encryption.js
+++ src/security/encryption.js
@@ -24,30 +24,28 @@ function encryptObject(x, password) {
 
 function encrypt(text, key) {
     if (!key) return
- let cipher = crypto.createCipher(algorithm, Buffer.from(key));
- let encrypted = cipher.update(text);
- encrypted = Buffer.concat([encrypted, cipher.final()]);
- return { encryptedData: encrypted.toString('hex') };
+    let cipher = crypto.createCipher(algorithm, Buffer.from(key));
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return { encryptedData: encrypted.toString('hex') };
 }
 
 function decrypt(text, key, isFile) {
     if (!text.encryptedData && !isFile) return text
     if (!key) return 'cannot decrypt'
- let encryptedText = Buffer.from(isFile ? text : text.encryptedData, 'hex');
- let decipher = crypto.createDecipher('aes-256-cbc', Buffer.from(key));
- let decrypted = decipher.update(encryptedText);
- decrypted = Buffer.concat([decrypted, decipher.final()]);
- return decrypted.toString();
+    let encryptedText = Buffer.from(isFile ? text : text.encryptedData, 'hex');
+    let decipher = crypto.createDecipher('aes-256-cbc', Buffer.from(key));
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
 }
 
 function stringifyToken(request) {
-    console.log('stringifying token ', request)
-  return encrypt(JSON.stringify(request), constants.EX2_ENCRYPTION_KEY) 
+    return encrypt(JSON.stringify(request), constants.EX2_ENCRYPTION_KEY) 
 }
 
 function parseToken(stringified) {
     const decrypted = decrypt(stringified, constants.EX2_ENCRYPTION_KEY)
-    console.log('decrypted is ', decrypted)
     return JSON.parse(decrypted)
   }
 
